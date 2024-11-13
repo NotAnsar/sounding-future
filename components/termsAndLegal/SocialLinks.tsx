50fca14f6aec759f4bee89c3cb5af1a37d78bd05
NotAnsar <karrouach.ansar@gmail.com>
2024-11-13 13:00:37 +0100

    mastodon color fixed

--- components/termsAndLegal/SocialLinks.tsx
+++ components/termsAndLegal/SocialLinks.tsx
@@ -25,7 +25,7 @@ export default function SocialLinks() {
 					<Icons.linkedin className='w-8 h-auto aspect-square text-foreground cursor-pointer hover:text-primary-foreground transition-colors duration-200 ease-out' />
 				</Link>
 				<Link href={'https://mastodon.social/@soundingfuture'} target='_blank'>
-					<Icons.mastodon className='w-8 h-auto aspect-square text-foreground cursor-pointer hover:text-primary-foreground transition-colors duration-200 ease-out' />
+					<Icons.mastodon className='w-8 h-auto aspect-square text-foreground fill-foreground cursor-pointer hover:text-primary-foreground transition-colors duration-200 ease-out' />
 				</Link>
 			</div>
 		</div>
