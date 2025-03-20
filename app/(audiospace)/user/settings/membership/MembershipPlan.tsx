081e45a659f8b4a37e8f123cb17a44971d187358
NotAnsar <karrouach.ansar@gmail.com>
2025-03-20 00:08:16 +0000

    membership edits done

--- app/(audiospace)/user/settings/membership/MembershipPlan.tsx
+++ app/(audiospace)/user/settings/membership/MembershipPlan.tsx
@@ -1,74 +1,3 @@
-// import { buttonVariants } from '@/components/ui/button';
-// import { cn } from '@/lib/utils';
-// import Link from 'next/link';
-// import React from 'react';
-
-// export default function MembershipPlan({ isPro }: { isPro: boolean }) {
-// 	return (
-// 		<div>
-// 			<h2 className='text-xl font-semibold mb-4'>Membership Plan</h2>
-// 			<div className='flex gap-4 mb-3'>
-// 				<div
-// 					className={cn(
-// 						'flex-1 p-5 border rounded-lg relative',
-// 						!isPro && 'border-primary border-2'
-// 					)}
-// 				>
-// 					{!isPro && (
-// 						<span className='absolute -top-2 left-4 px-2 py-0.5 text-xs font-medium bg-background border border-primary text-primary rounded-sm'>
-// 							Current
-// 						</span>
-// 					)}
-// 					<h3 className='text-base font-medium text-center'>Free</h3>
-// 				</div>
-
-// 				<div
-// 					className={cn(
-// 						'flex-1 p-5 border rounded-lg relative',
-// 						isPro && 'border-primary border-2'
-// 					)}
-// 				>
-// 					{isPro ? (
-// 						<span className='absolute -top-2 left-4 px-2 py-0.5 text-xs font-medium bg-background border border-primary text-primary rounded-sm'>
-// 							Current
-// 						</span>
-// 					) : (
-// 						<span className='absolute -top-2 right-4 px-2 py-0.5 text-xs font-medium bg-background border border-primary text-primary rounded-sm'>
-// 							Recommended
-// 						</span>
-// 					)}
-// 					<h3 className='text-base font-medium text-center '>Pro</h3>
-// 					{!isPro && (
-// 						<Link
-// 							className={cn(buttonVariants({ size: 'sm' }), 'w-full mt-2')}
-// 							href={'https://buy.stripe.com/9AQ4id4eB05P8OkcMM'}
-// 							target='_blank'
-// 						>
-// 							Upgrade
-// 						</Link>
-// 					)}
-// 				</div>
-// 			</div>
-
-// 			{!isPro && (
-// 				<div className='mt-6'>
-// 					<p className='mb-3'>
-// 						Learn more how you can support us as Pro user and get advanced
-// 						platform features.
-// 					</p>
-// 					<Link
-// 						className={cn(buttonVariants(), 'font-semibold')}
-// 						href={'https://buy.stripe.com/9AQ4id4eB05P8OkcMM'}
-// 						target='_blank'
-// 					>
-// 						Support Us
-// 					</Link>
-// 				</div>
-// 			)}
-// 		</div>
-// 	);
-// }
-
 import { buttonVariants } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
@@ -129,7 +58,7 @@ export default function MembershipPlan({ isPro }: { isPro: boolean }) {
 					</p>
 					<Link
 						className={cn(buttonVariants(), 'font-semibold')}
-						href={'https://buy.stripe.com/9AQ4id4eB05P8OkcMM'}
+						href='/support-us'
 						target='_blank'
 					>
 						Support Us
